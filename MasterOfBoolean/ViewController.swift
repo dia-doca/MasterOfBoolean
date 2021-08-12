fd17e8f5cf5c4e452f27043f6df166d265a59778
Ivan Druzhinin <dia.doca@gmail.com>
2021-08-12 23:30:17 +0300

    Initial Commit

---
+++ MasterOfBoolean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MasterOfBoolean
+//
+//  Created by Ivan Druzhinin on 12.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
